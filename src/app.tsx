2110967fff77dbab3ccd442885ded8ffe8d0ecfc
Bernhart46 <hegyiferenc46@gmail.com>
2023-05-20 09:59:16 +0200

    Add basic routing

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,16 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<h1>Home</h1>} />
+        <Route path="/projects" element={<h1>Projects</h1>} />
+        <Route path="/skills" element={<h1>Skills</h1>} />
+        <Route path="/contacts" element={<h1>Contacts</h1>} />
+        <Route path="*" element={<Navigate to="/home" />} />
+      </Routes>
+    </>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./app";
+import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.querySelector("#root") as HTMLDivElement;
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
