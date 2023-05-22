57681fd89ce4c643e28e6263d800897e41ecf281
Bernhart46 <hegyiferenc46@gmail.com>
2023-05-22 09:55:31 +0200

    Add assets (menu-icons)

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+import homeIcon from "./home-icon.svg";
+import projectsIcon from "./projects-icon.svg";
+import skillsIcon from "./skills-icon.svg";
+import contactsIcon from "./contacts-icon.svg";
+
+export const exportObject = {
+  homeIcon,
+  projectsIcon,
+  skillsIcon,
+  contactsIcon,
+};
