75f9f3c283bf846e3a839f84e8520a3f590f8cc8
Bernhart46 <hegyiferenc46@gmail.com>
2023-05-22 15:44:03 +0200

    Remove unused empty file

--- src/components/nav/other-navs.tsx
+++
@@ -1,7 +0,0 @@
-import { useLocation } from "react-router-dom";
-import "./index.scss";
-import NavButtonComponent, {
-  chooseMenuIcon,
-} from "./nav-button/nav-button-component";
-
-//Mobile Only
