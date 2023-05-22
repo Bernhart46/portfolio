60190023e1a454d3b47dc6fb7af5cbe6b43fe8f9
Bernhart46 <hegyiferenc46@gmail.com>
2023-05-22 15:28:32 +0200

    Add useMobileDetect CustomHook

---
+++ src/hooks/useMobileDetect.ts
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export const useMobileDetect = () => {
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  useEffect(() => {
+    const setMobileStatus = () => {
+      const condition = window.innerWidth < 768;
+      setIsMobile(condition);
+    };
+    window.addEventListener("resize", setMobileStatus);
+
+    return () => window.removeEventListener("resize", setMobileStatus);
+  }, []);
+
+  return isMobile;
+};
