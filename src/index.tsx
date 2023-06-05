0678f2eb5ca4d683e54ce2563b4d05de193a5d79
Bernhart46 <hegyiferenc46@gmail.com>
2023-06-05 13:42:32 +0200

    Fix height issues on mobile (100vh -> innerHeight)

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import App from "./app";
 import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.querySelector("#root") as HTMLDivElement;
+rootElement.style.height = `${window.innerHeight}px`;
 const root = createRoot(rootElement);
 
 root.render(
