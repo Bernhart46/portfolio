fe506368677998b8d2fd3aa99b4f916e01b8b1df
Bernhart46 <hegyiferenc46@gmail.com>
2023-09-06 17:27:32 +0200

    Fix Loading Text
    This bugfix fixes an issue with the Loading Text's visibility. The loading text in certain sizes was too large for the animation, so I fixed it so that if the ring container's height is less than 150, it doesn't show the text.

--- src/components/loading-screen/loading-screen-component.tsx
+++ src/components/loading-screen/loading-screen-component.tsx
@@ -1,9 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 import "./loading-screen-component.scss";
 
 export function LoadingScreenComponent() {
   const [isLoading, setIsLoading] = useState(true);
+  const loadingRingRef = useRef(null);
+  const [isBigForShowText, setIsBigForShowText] = useState(false);
 
   const LOADING_TIME_SECONDS = 3;
 
@@ -16,14 +18,36 @@ export function LoadingScreenComponent() {
     return () => clearTimeout(loadingTimeout);
   }, []);
 
+  useEffect(() => {
+    const resizeWhileLoading = () => {
+      if (loadingRingRef.current) {
+        const loadingRingDiv = loadingRingRef.current as HTMLDivElement;
+
+        const isBig = loadingRingDiv.offsetWidth > 150;
+
+        if (isBig !== isBigForShowText) {
+          setIsBigForShowText(isBig);
+        }
+      }
+    };
+
+    resizeWhileLoading();
+
+    window.addEventListener("resize", resizeWhileLoading);
+
+    return () => {
+      window.removeEventListener("resize", resizeWhileLoading);
+    };
+  }, [isBigForShowText]);
+
   return createPortal(
     isLoading && (
       <section className="loading-screen-component">
-        <div className="loading-ring">
+        <div className="loading-ring-container" ref={loadingRingRef}>
           <div className="outer-ring">
             <div className="inner-ring"></div>
           </div>
-          <span>Loading...</span>
+          {isBigForShowText && <span>Loading...</span>}
         </div>
       </section>
     ),
