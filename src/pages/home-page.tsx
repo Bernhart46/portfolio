aad4a6fc61c4390c8662c76f5fdd3f86fb1d114a
Bernhart46 <hegyiferenc46@gmail.com>
2023-09-07 11:58:03 +0200

    Update home-page.tsx
    I made the JSX part of the home-page component more readable.

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -6,23 +6,33 @@ export function HomePageComponent() {
   const { primary_title, secondary_title, about_me } =
     useContext(languageContext)?.home_page || {};
 
-  return (
-    <section className="home-page__component">
+  const homePageTitleElement = (
+    <>
       <div className="home-page__title__background"></div>
       <div className="home-page__title__container">
         <h1>{primary_title}</h1>
         <h3>{secondary_title}</h3>
       </div>
-      <div className="home-page__aboutme__container">
-        <div className="home-page__aboutme__title">
-          <span>{about_me?.title}</span>
-        </div>
-        <div className="home-page__aboutme__text">
-          {about_me?.texts.map((paragraph, index) => {
-            return <p key={index}>{paragraph}</p>;
-          })}
-        </div>
+    </>
+  );
+
+  const homePageAboutmeElement = (
+    <div className="home-page__aboutme__container">
+      <div className="home-page__aboutme__title">
+        <span>{about_me?.title}</span>
       </div>
+      <div className="home-page__aboutme__text">
+        {about_me?.texts.map((paragraph, index) => {
+          return <p key={index}>{paragraph}</p>;
+        })}
+      </div>
+    </div>
+  );
+
+  return (
+    <section className="home-page__component">
+      {homePageTitleElement}
+      {homePageAboutmeElement}
     </section>
   );
 }
