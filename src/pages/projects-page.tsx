d6840bcf086cfa352fcfa30fb7483a3902d4718e
Bernhart46 <hegyiferenc46@gmail.com>
2023-09-11 09:10:35 +0200

    Add Title to the Projects page

--- src/pages/projects-page.tsx
+++ src/pages/projects-page.tsx
@@ -6,10 +6,15 @@ export function ProjectsPageComponent() {
   const { primary_title, secondary_title } =
     useContext(languageContext)?.projects_page || {};
 
-  return (
-    <section className="projects-page-component">
-      <h1>{primary_title}</h1>
-      <h3>{secondary_title}</h3>
-    </section>
+  const projectsPageTitleElement = (
+    <div className="projects-page-component">
+      <div className="projects-page__title__background"></div>
+      <div className="projects-page__title__container">
+        <h1>{primary_title}</h1>
+        <h3>{secondary_title}</h3>
+      </div>
+    </div>
   );
+
+  return projectsPageTitleElement;
 }
