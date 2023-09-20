f0ce853c922dd1489349e45500e95d3e120ced34
Bernhart46 <hegyiferenc46@gmail.com>
2023-09-20 13:50:05 +0200

    Rework QA multi-line style
    I just found an easier approach; I don't know why I hadn't thought about this earlier.

--- src/components/question-answer/question-answer-component.tsx
+++ src/components/question-answer/question-answer-component.tsx
@@ -1,47 +1,18 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import "./question-answer-component.scss";
 import { QuestionAnswerComponentProps } from "./question-answer-types";
 
 export function QuestionAnswerComponent(props: QuestionAnswerComponentProps) {
   const { question, answer } = props;
-  const questionRef = useRef<HTMLDivElement>(null);
   const [isOpen, setIsOpen] = useState(false);
-  const [isMultipleLine, setIsMultipleLine] = useState(false);
 
   const toggleIsOpen = () => {
     setIsOpen(!isOpen);
   };
 
-  const setHeightOnChange = () => {
-    if (!questionRef || !questionRef.current) return;
-    const height = questionRef.current.offsetHeight;
-
-    if (height > 40) {
-      setIsMultipleLine(true);
-    } else {
-      setIsMultipleLine(false);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("load", setHeightOnChange);
-    window.addEventListener("resize", setHeightOnChange);
-
-    return () => {
-      window.removeEventListener("load", setHeightOnChange);
-      window.removeEventListener("resize", setHeightOnChange);
-    };
-  }, []);
-
   return (
     <div className="question-answer-component">
-      <div
-        className={`question-part ${
-          isMultipleLine ? "question-multiline" : ""
-        }`}
-        onClick={toggleIsOpen}
-        ref={questionRef}
-      >
+      <div className={"question-part"} onClick={toggleIsOpen}>
         {question}
       </div>
       <div
