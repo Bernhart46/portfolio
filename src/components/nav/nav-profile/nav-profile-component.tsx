b22e234e1a4bebb653ad5c21c4c5ab5a057238ca
Bernhart46 <hegyiferenc46@gmail.com>
2023-09-20 09:34:19 +0200

    Change picture imports
    I changed the method of picture importing. Now they are all in the SCSS files.
    I removed the profile loading feature because it was unnecessary and because there is already a loading feature.

--- src/components/nav/nav-profile/nav-profile-component.tsx
+++ src/components/nav/nav-profile/nav-profile-component.tsx
@@ -1,26 +1,14 @@
-import { useContext, useState } from "react";
-import profilePicture from "../../../../public/pictures/profile.png";
+import { useContext } from "react";
 import "./nav-profile-component.scss";
 import { languageContext } from "../../../shared/contexts";
 
 export function NavProfileComponent() {
-  const [isLoading, setIsLoading] = useState(true);
   const { profile } = useContext(languageContext)?.nav || {};
-
-  const handleOnLoad = () => {
-    setIsLoading(false);
-  };
 
   return (
     <section className="nav__profile__component">
       <div className="nav__profile-picture__container">
-        <img
-          className="nav__profile-picture"
-          src={profilePicture}
-          alt={profile?.alt}
-          onLoad={handleOnLoad}
-          style={{ opacity: isLoading ? "0" : "1" }}
-        />
+        <div className="nav__profile-picture"></div>
       </div>
       <div className="nav__profile-name__container">
         <h1 className="nav__profile-name">{profile?.name}</h1>
