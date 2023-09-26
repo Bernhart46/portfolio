150a8e7e027ab349068e6cc26ccbfb00f7c03531
Bernhart46 <hegyiferenc46@gmail.com>
2023-09-26 14:31:26 +0200

    Add Template project-components
    I added templates and made the list responsive.

--- src/components/project/project-component.tsx
+++ src/components/project/project-component.tsx
@@ -1,5 +1,39 @@
 import "./project-component.scss";
 
 export function ProjectComponent() {
-  return <section className="project-component"></section>;
+  return (
+    <section className="project-component">
+      <div className="project__container">
+        <div className="project__left-side">
+          <div className="project-mobile-title">Példa név</div>
+          <div className="project-picture"></div>
+        </div>
+        <div className="project__right-side">
+          <div className="project-desktop-title">Példa név</div>
+          <div className="project-description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus laudantium numquam incidunt quo atque magni delectus
+            officiis, exercitationem eveniet? Consequuntur quam beatae suscipit
+            illum,Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus laudantium numquam incidunt quo atque magni delectus
+            officiis, exercitationem eveniet? Consequuntur quam beatae suscipit
+            illum,Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus laudantium numquam incidunt quo atque magni delectus
+            officiis, exercitationem eveniet? Consequuntur quam beatae suscipit
+            illum,Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus laudantium numquam incidunt quo atque magni delectus
+            officiis, exercitationem eveniet? Consequuntur quam beatae suscipit
+            illum,Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus laudantium numquam incidunt quo atque magni delectus
+            officiis, exercitationem eveniet? Consequuntur quam beatae suscipit
+            illum,Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus laudantium numquam incidunt quo atque magni delectus
+            officiis, exercitationem eveniet? Consequuntur quam beatae suscipit
+            illum,
+          </div>
+        </div>
+      </div>
+      <div className="project-tags">HTML, CSS, JavaScript, Node</div>
+    </section>
+  );
 }
